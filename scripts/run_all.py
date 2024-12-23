f28d2cfc0b36a6c2746a5dba6fcfd7cd2334dc87
drbenn <bennett.daniel@gmail.com>
2024-12-22 21:29:59 -0500

    fix py start script

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -2,13 +2,13 @@ import subprocess
 
 # Open first command line, navigate to api-todo-nest directory, and run 'npm run start:dev'
 api_todo_nest_process = subprocess.Popen(
-    ["npm", "run", "start:dev"], cwd="api-todo-nest", shell=True
+    ["npm", "run", "start:dev"], cwd="../api-todo-nest", shell=True
 )
 print("Started api-todo-nest process.")
 
 # Open another command line, navigate to UI directory, and run 'npm run start'
 ui_ang_19_process = subprocess.Popen(
-    ["npm", "run", "start"], cwd="ui-ang-19", shell=True
+    ["npm", "run", "start"], cwd="../ui-ang-19", shell=True
 )
 print("Started ui-ang-19 process.")
 
