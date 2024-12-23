1ae6dd7b7de1efce01f6e5c5ce5b3e6595abfe54
drbenn <bennett.daniel@gmail.com>
2024-12-22 21:40:13 -0500

    fix typeorm repo import error

--- api-todo-nest/src/app.module.ts
+++ api-todo-nest/src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
 import { TodoModule } from './todo/todo.module';
+import { Todo } from './todo/todo.entity';
 
 @Module({
   imports: [
@@ -13,14 +14,15 @@ import { TodoModule } from './todo/todo.module';
       host: 'localhost', // Database host
       port: 3306, // MySQL port
       username: 'root', // Your MySQL username
-      password: 'password', // Your MySQL password
-      database: 'todo_db', // Your database name
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to your entities
+      password: 'pass', // Your MySQL password
+      database: 'todo', // Your database name
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to your entities
+      entities: [Todo], // Path to your entities
       // synchronize: true, // Auto-create database schema (disable in production)
     }),
     TodoModule,
   ],
-  controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
